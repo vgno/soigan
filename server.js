54daa041de6567216ee0a19dbb2e9df0c98e7860
Espen Volden <voldern@hoeggen.net>
2012-08-08 14:51:36 +0200

    Basic implementation of uname

--- server.js
+++ server.js
@@ -7,13 +7,31 @@ var io = require('socket.io').listen(server);
 server.listen(3000);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+    res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+    socket.on('yo', function(data) {
+        if (data.type == 'client') {
+            socket.join('clients');
+            socket.emit('status', 'ok');
+            
+            console.log('Got new client');
+        } else if (data.type == 'browser') {
+            socket.join('browsers');
+            socket.emit('status', 'ok');
+        }
+    });
+
+    socket.on('uname', function(data) {
+        console.log('Sending uname to browsers');
+        io.sockets.in('browsers').emit('uname', data);
+    });
+
+    socket.on('send', function(data) {
+        console.log('Sending', data.cmd);
+        
+        io.sockets.in('clients').emit(data.cmd);
+    });
 });
 
